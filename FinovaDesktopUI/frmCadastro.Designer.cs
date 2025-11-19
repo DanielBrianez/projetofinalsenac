924dd10744199cfe7baf57616804665a77404f8e
MARYANA OLIVEIRA DOS SANTOS <mlopeslucariello@gmail.com>
2025-11-18 21:02:20 -0300

    new: criação das dashboards e telas de login/cadastro.

---
+++ FinovaDesktopUI/frmCadastro.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace FinnovaDesktopUI
+{
+    partial class frmCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastro));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            pictureBox1 = new PictureBox();
+            txtNome = new Guna.UI2.WinForms.Guna2TextBox();
+            txtSenha = new Guna.UI2.WinForms.Guna2TextBox();
+            txtLogin = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            guna2DateTimePicker1 = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            btnGoogle = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1150, 750);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // txtNome
+            // 
+            txtNome.BackColor = Color.White;
+            txtNome.BorderColor = Color.White;
+            txtNome.CustomizableEdges = customizableEdges1;
+            txtNome.DefaultText = "";
+            txtNome.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtNome.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtNome.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtNome.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtNome.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtNome.Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtNome.ForeColor = Color.White;
+            txtNome.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtNome.Location = new Point(644, 153);
+            txtNome.Margin = new Padding(3, 4, 3, 4);
+            txtNome.Name = "txtNome";
+            txtNome.PlaceholderText = "";
+            txtNome.SelectedText = "";
+            txtNome.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            txtNome.Size = new Size(394, 42);
+            txtNome.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            txtSenha.BackColor = Color.White;
+            txtSenha.BorderColor = Color.White;
+            txtSenha.CustomizableEdges = customizableEdges3;
+            txtSenha.DefaultText = "";
+            txtSenha.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtSenha.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtSenha.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSenha.ForeColor = Color.White;
+            txtSenha.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Location = new Point(644, 416);
+            txtSenha.Margin = new Padding(3, 4, 3, 4);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PlaceholderText = "";
+            txtSenha.SelectedText = "";
+            txtSenha.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            txtSenha.Size = new Size(394, 42);
+            txtSenha.TabIndex = 3;
+            // 
+            // txtLogin
+            // 
+            txtLogin.BackColor = Color.White;
+            txtLogin.BorderColor = Color.White;
+            txtLogin.CustomizableEdges = customizableEdges5;
+            txtLogin.DefaultText = "";
+            txtLogin.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtLogin.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtLogin.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtLogin.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtLogin.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtLogin.Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtLogin.ForeColor = Color.White;
+            txtLogin.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtLogin.Location = new Point(644, 328);
+            txtLogin.Margin = new Padding(3, 4, 3, 4);
+            txtLogin.Name = "txtLogin";
+            txtLogin.PlaceholderText = "";
+            txtLogin.SelectedText = "";
+            txtLogin.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtLogin.Size = new Size(394, 42);
+            txtLogin.TabIndex = 4;
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.CustomizableEdges = customizableEdges7;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.FromArgb(9, 20, 137);
+            guna2Button1.Font = new Font("Poppins", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(644, 478);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Button1.Size = new Size(394, 45);
+            guna2Button1.TabIndex = 6;
+            guna2Button1.Text = "Cadastrar";
+            // 
+            // guna2CircleButton1
+            // 
+            guna2CircleButton1.BackColor = Color.FromArgb(0, 4, 32);
+            guna2CircleButton1.DisabledState.BorderColor = Color.DarkGray;
+            guna2CircleButton1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2CircleButton1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2CircleButton1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2CircleButton1.FillColor = Color.FromArgb(9, 20, 137);
+            guna2CircleButton1.Font = new Font("Segoe UI", 9F);
+            guna2CircleButton1.ForeColor = Color.White;
+            guna2CircleButton1.Location = new Point(1098, 12);
+            guna2CircleButton1.Name = "guna2CircleButton1";
+            guna2CircleButton1.ShadowDecoration.CustomizableEdges = customizableEdges9;
+            guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CircleButton1.Size = new Size(40, 40);
+            guna2CircleButton1.TabIndex = 7;
+            guna2CircleButton1.Text = "X";
+            // 
+            // guna2DateTimePicker1
+            // 
+            guna2DateTimePicker1.Checked = true;
+            guna2DateTimePicker1.CustomizableEdges = customizableEdges10;
+            guna2DateTimePicker1.FillColor = SystemColors.Window;
+            guna2DateTimePicker1.Font = new Font("Segoe UI", 9F);
+            guna2DateTimePicker1.Format = DateTimePickerFormat.Long;
+            guna2DateTimePicker1.Location = new Point(644, 240);
+            guna2DateTimePicker1.MaxDate = new DateTime(9998, 12, 31, 0, 0, 0, 0);
+            guna2DateTimePicker1.MinDate = new DateTime(1753, 1, 1, 0, 0, 0, 0);
+            guna2DateTimePicker1.Name = "guna2DateTimePicker1";
+            guna2DateTimePicker1.ShadowDecoration.CustomizableEdges = customizableEdges11;
+            guna2DateTimePicker1.Size = new Size(394, 43);
+            guna2DateTimePicker1.TabIndex = 8;
+            guna2DateTimePicker1.Value = new DateTime(2025, 11, 18, 20, 47, 46, 708);
+            // 
+            // btnGoogle
+            // 
+            btnGoogle.BorderColor = Color.DimGray;
+            btnGoogle.CustomizableEdges = customizableEdges12;
+            btnGoogle.DisabledState.BorderColor = Color.DarkGray;
+            btnGoogle.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnGoogle.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnGoogle.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnGoogle.FillColor = Color.White;
+            btnGoogle.Font = new Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGoogle.ForeColor = Color.Black;
+            btnGoogle.Image = (Image)resources.GetObject("btnGoogle.Image");
+            btnGoogle.ImageSize = new Size(100, 35);
+            btnGoogle.Location = new Point(647, 561);
+            btnGoogle.Name = "btnGoogle";
+            btnGoogle.ShadowDecoration.CustomizableEdges = customizableEdges13;
+            btnGoogle.Size = new Size(391, 42);
+            btnGoogle.TabIndex = 9;
+            // 
+            // frmCadastro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1150, 750);
+            Controls.Add(btnGoogle);
+            Controls.Add(guna2DateTimePicker1);
+            Controls.Add(guna2CircleButton1);
+            Controls.Add(guna2Button1);
+            Controls.Add(txtLogin);
+            Controls.Add(txtSenha);
+            Controls.Add(txtNome);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmCadastro";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2TextBox txtNome;
+        private Guna.UI2.WinForms.Guna2TextBox txtSenha;
+        private Guna.UI2.WinForms.Guna2TextBox txtLogin;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2DateTimePicker guna2DateTimePicker1;
+        private Guna.UI2.WinForms.Guna2Button btnGoogle;
+    }
+}
